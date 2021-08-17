a6ae9d4aec648fef7789bc188de75681265560f9
Les Matheson <lmatheson4@bloomberg.net>
2021-08-17 07:59:28 -0400

    Update localhist load process

---
+++ bin/localhist-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Running localhist-version.sh is the correct way to
+# get the home path for localhist and its tools.
+LocalhistVer=0.8.0
+
+set -e
+
+Script=$(readlink -f "$0")
+Scriptdir=$(dirname -- "$Script")
+
+
+if [ -z "$sourceMe" ]; then
+    printf "%s\t%s" ${Scriptdir}/localhist ${LocalhistVer} 
+fi
