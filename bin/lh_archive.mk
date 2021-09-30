977e90ffff17e6a33be466d37d7a661c248be0df
Les Matheson <lmatheson4@bloomberg.net>
2021-09-30 07:20:21 -0400

    Cleanup lh_archive.mk output

--- bin/lh_archive.mk
+++ bin/lh_archive.mk
@@ -10,17 +10,17 @@ HISTFILES = $(filter-out root, $(wildcard *))
 TGTFILES = $(patsubst %, $(LH_ARCHIVE)/$(HOST)/%, $(HISTFILES))
 #$(info HISTFILES=$(HISTFILES))
 #$(info TGTFILES=$(TGTFILES))
-$(info HOST=$(HOST))
+#$(info HOST=$(HOST))
 
 .PHONY: all
 
 all: $(TGTFILES)
 
 $(TGTFILES): $(LH_ARCHIVE)/$(HOST)/% : %
-	[[ -n $$LH_ARCHIVE ]] || { echo "Error: LH_ARCHIVE not defined"; exit 1; }
-	[[ -n $(HOST) ]] || { echo "Fail: HOST not set" >&2; exit 1; }
-	mkdir -p $(LH_ARCHIVE)/$(HOST)
-	echo Copying $<:
+	@[[ -n $$LH_ARCHIVE ]] || { echo "Error: LH_ARCHIVE not defined"; exit 1; }
+	@[[ -n $(HOST) ]] || { echo "Fail: HOST not set" >&2; exit 1; }
+	@mkdir -p $(LH_ARCHIVE)/$(HOST)
+	@echo Copying $<:
 	cp $< $(LH_ARCHIVE)/$(HOST)/
 	$(LH_BIN)/localhist-cleanup.sh $(LH_ARCHIVE)/$(HOST)/$<
 
