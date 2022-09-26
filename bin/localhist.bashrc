692c69391157616f743421fe9f9833e82e43b965
Les Matheson <lmatheson4@bloomberg.net>
2022-09-26 08:10:02 -0400

    Initial scaffolding from shellkit:0.8.17

---
+++ bin/localhist.bashrc
@@ -0,0 +1,11 @@
+# localhist.bashrc - shell init file for localhist sourced from ~/.bashrc
+
+localhist-semaphore() {
+    [[ 1 -eq  1 ]]
+}
+
+do-localhist() {
+    # TODO: This is a test function.  You can discard it.
+    localhist.sh
+}
+
---
+++ make-kit.mk
@@ -0,0 +1,23 @@
+# make-kit.mk for localhist
+#  This makefile is included by the root shellkit Makefile
+#  It defines values that are kit-specific.
+#  You should edit it and keep it source-controlled.
+
+# TODO: update kit_depends to include anything which
+#   might require the kit version to change as seen
+#   by the user -- i.e. the files that get installed,
+#   or anything which generates those files.
+kit_depends := \
+    bin/localhist.bashrc \
+    bin/localhist.sh
+
+.PHONY: publish
+
+
+publish: pre-publish publish-common release-draft-upload release-list
+
+
+	@echo ">>>> publish complete OK.  <<<"
+	@echo ">>>> Manually publish the release from this URL when satisfied, <<<<"
+	@echo ">>>> and then change ./version to avoid accidental confusion. <<<<"
+	cat tmp/draft-url
