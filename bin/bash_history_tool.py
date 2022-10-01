5c1793f471e76ede6c6cd7202835de80b30aaed0
Les Matheson <lmatheson4@bloomberg.net>
2022-10-01 10:47:48 +0000

    sync

--- bin/bash_history_tool.py
+++ bin/bash_history_tool.py
@@ -19,73 +19,125 @@
 #
 
 
-
+from audioop import add
+from bisect import bisect_left
 import sys
 import os
-from typing import Iterable, OrderedDict, List
+from typing import Iterable, OrderedDict, List, Callable, Tuple
 from collections import OrderedDict
 import re
 from datetime import datetime
 from io import TextIOWrapper
 
-testdata_1='''
-#1628246305
-ls
-#1628246312
-rm -rf ELT-2021-Jul-26-LF-NY/ # comment
-#1628246324
-cd
-#1628216330
-mv .bash_logout bash_logout # comment
-#1628246333
-'''
+RE_TIMESTAMP = re.compile("^#\d+\s*$")
 
-RE_TIMESTAMP=re.compile('^#\d+\s*$')
 
-def get_events_reversed(text:str) -> str:
-    ''' Yield one event at a time in reverse order.'''
+def get_events_reversed(text: str) -> str:
+    """Yield one event at a time in reverse order."""
     for line in reversed(text.splitlines()):
         if line:
             yield line
 
+
 class Context:
     def __init__(self):
-        self.input_files:List[str]=[]
-        self.output_dir:str=None
+        self.input_files: List[str] = []
+        self.output_dir: str = None
+
 
 class CoalesceState:
     def __init__(self):
-        self.buckets={}
+        self.buckets = {}
+
 
 class LogEvent:
-    def __init__(self,timestamp:datetime= None,msg:str=None):
-        self.msg:string = msg
-        self.timestamp:datetime = timestamp
+    def __init__(self, timestamp: datetime = None, msg: str = None):
+        self.msg: string = msg
+        self.timestamp: datetime = timestamp
 
     def __repr__(self):
         return f"#{int(self.timestamp.timestamp())}\n{self.msg}"
+
+
+class Bucket:
+    """Represents a single event bucket like 2021-09, with events"""
+
+    def __init__(self, bucket_name: str):
+        self.bucket_name = bucket_name
+        """ A bucket_name can be anything, but the archived buckets
+        will have names like "YYYY-MM" representing the year and month
+        of the event records """
+        self.events: List[LogEvent] = []
+
+
+class BucketFarm:
+    """A set of buckets with distinct ordered names"""
+
+    def __init__(self):
+        self._buckets:List[Bucket] = []
+        """ _buckets is ordered by .bucket_name at all times """
+        self._bucket_names:List[int] = []
+        """ _bucket_names is an ordered list of bucket names, so we're optimized for
+        search with this data duplication. """
+
+    def _add_bucket(self, bucket_name: str) -> Tuple[Bucket,int, bool]:
+        """ Add a bucket with the given name if it doesn't exist.
+        New bucket will be ordered by bucket_name.  Returns bucket and
+        index at which bucket can be found in the ._buckets list, whether
+        or not a new bucket was created. Last member of result tuple is
+        a bool indicating whether bucket was newly created. """
+        index = bisect_left(self._bucket_names, bucket_name )
+        added = False
+        if index >= len(self._bucket_names):
+            # Add to the end:
+            self._buckets.append(Bucket(bucket_name))
+            self._bucket_names.append(bucket_name)
+            added = True
+        elif self._bucket_names[index] == bucket_name:
+            # Found existing bucket with that name:
+            pass
+        else:
+            # Need to insert new bucket at index
+            self._bucket_names.insert(index,bucket_name)
+            self._buckets.insert(index,Bucket(bucket_name))
+            added = True
+        return self._buckets[index],index,added
+
+    def get_bucket(self, bucket_name: str) -> Bucket:
+        """Returns bucket with given name.  Creates it if not found"""
+        result = self._add_bucket(bucket_name)
+        return result[0]
+
+def load_bucket(bucket: Bucket, event_iter) -> Bucket:
+    """Load a bucket from an event stream, and order events by time"""
+    bucket.events += [e for e in event_iter]
+    bucket.events = sorted(bucket.events, key=lambda ev: ev.timestamp)
+    return bucket
 
 
 class Command:
     def __init__(self, command_text: str, timestamp: int = 0):
         self.command_text = command_text
         self.timestamp = timestamp
+
     def __hash__(self):
         return self.command_text.__hash__()
+
     def __str__(self):
         if self.timestamp:
-            return '\n'.join((f'#{self.timestamp}',self.command_text))
+            return "\n".join((f"#{self.timestamp}", self.command_text))
         return self.command_text
 
 
-def is_timestamp(line:str) -> bool:
+def is_timestamp(line: str) -> bool:
     if RE_TIMESTAMP.match(line):
         return True
     return False
 
-def load_raw_log( instream: TextIOWrapper ) -> LogEvent:
-    ''' Yield events from instream '''
-    curDate:datetime=datetime.now()
+
+def load_raw_log(instream: TextIOWrapper) -> LogEvent:
+    """Yield events from instream"""
+    curDate: datetime = datetime.now()
     while True:
         line = instream.readline()
         if not line:
@@ -96,7 +148,7 @@ def load_raw_log( instream: TextIOWrapper ) -> LogEvent:
             except Exception as e:
                 continue
         else:
-            yield LogEvent(curDate,line.rstrip())
+            yield LogEvent(curDate, line.rstrip())
 
 
 def condense_bash_history(
@@ -106,25 +158,26 @@ def condense_bash_history(
         cache = OrderedDict()
     for event in event_stream:
         if is_timestamp(event):
-            timestamp=int(event[1:])
+            timestamp = int(event[1:])
             try:
-                last_command=next(reversed(cache))
+                last_command = next(reversed(cache))
             except StopIteration:
                 continue
 
             if cache[last_command].timestamp == 0:
                 cache[last_command].timestamp = timestamp
         else:
-            if not '#' in event:
+            if not "#" in event:
                 continue
             command = Command(event)
             if command.command_text in cache:
                 continue
-            cache[event]=command
-    time_ordered = sorted(cache.values(),key=lambda x: x.timestamp)
+            cache[event] = command
+    time_ordered = sorted(cache.values(), key=lambda x: x.timestamp)
     return time_ordered
 
-def coalesce_events(context:Context, state:CoalesceState()) -> bool:
+
+def coalesce_events(context: Context, state: CoalesceState()) -> bool:
     """Read events from event_stream.  Input order is unimportant, except
     that we assume the timestamp precedes the correlated log record.  If
     we encounter log records with no timestamp, we just re-apply the most
@@ -142,6 +195,7 @@ def coalesce_events(context:Context, state:CoalesceState()) -> bool:
     """
     pass
 
+
 # def cleanup_stream(instream,outstream):
 #     """ obsolete function """
 #     ''' Reorder, remove dupes and uncommented, ensure uniqueness '''
@@ -149,17 +203,18 @@ def coalesce_events(context:Context, state:CoalesceState()) -> bool:
 #         outstream.write(str(event))
 #         outstream.write('\n')
 
-def parse_args(argv:List[str]) -> Context:
-    ctx=Context()
-    mode="undefined"
+
+def parse_args(argv: List[str]) -> Context:
+    ctx = Context()
+    mode = "undefined"
     try:
-        for ndx,arg in enumerate(argv):
-            if arg == '--input':
-                ctx.input_files.append(argv[ndx+1])
+        for ndx, arg in enumerate(argv):
+            if arg == "--input":
+                ctx.input_files.append(argv[ndx + 1])
             elif arg == "--output":
-                ctx.output_dir = argv[ndx+1]
+                ctx.output_dir = argv[ndx + 1]
             elif arg == "--mode":
-                mode=argv[ndx+1]
+                mode = argv[ndx + 1]
 
         if not mode:
             raise RuntimeError("No --mode specified")
@@ -168,13 +223,13 @@ def parse_args(argv:List[str]) -> Context:
         if not ctx.input_files:
             ctx.input_files.append("/dev/stdin")
         if not ctx.output_dir:
-            ctx.output_dir=f"{os.environ.get('HOME','/')}/.localhist-archive"
+            ctx.output_dir = f"{os.environ.get('HOME','/')}/.localhist-archive"
 
     except ex as Exception:
         raise RuntimeError(f"Can't parse command line {argv}: {ex}")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     context = parse_args(sys.argv)
     state = CoalesceState()
     sys.exit(0)
--- test/test_bash_history_tool.py
+++ test/test_bash_history_tool.py
@@ -1,7 +1,12 @@
 # test_bash_history_tool.py
 import sys
+from datetime import datetime
 import os
-sys.path.insert(0,f"{os.getcwd()}/bin")
+import random
+
+
+LH_ROOT = os.environ["LH_ROOT"]  # Should point to the localhist/ dir
+sys.path.insert(0, LH_ROOT)
 import io
 from bash_history_tool import *
 
@@ -14,12 +19,54 @@ tmk trainee-prs
 #1664572672
 tmk trainee-prs > trainee-pr-urls"""
 
+SYNTH_FAKE_90DAYS = f"{LH_ROOT}/test/home1/synth-fake-90days"
+
+
 def test_load_raw_log():
     stream = io.StringIO(SAMPLE_LOG_1)
-    vv = [ r for r in load_raw_log( stream ) ]
+    vv = [r for r in load_raw_log(stream)]
     assert len(vv) == 4
-    assert 'pr-urls' in vv[3].msg
+    assert "pr-urls" in vv[3].msg
+
+
+def test_fake_90days():
+    with open(SYNTH_FAKE_90DAYS, "r") as stream:
+        vv = [r for r in load_raw_log(stream)]
+    assert len(vv) == 90
+    assert "90 days old" in vv[0].msg
+    assert vv[0].timestamp == datetime.fromtimestamp(1656812544)
+    assert "1 days old" in vv[89].msg
+    assert vv[89].timestamp == datetime.fromtimestamp(1664502144)
+    stream.close
+
+
+def test_load_bucket():
+    with open(SYNTH_FAKE_90DAYS, "r") as stream:
+        bucket = load_bucket(Bucket("raw"), load_raw_log(stream))
+    random.shuffle(bucket.events)
+    bucket = load_bucket(Bucket("sorted"), bucket.events)
+    # Prove it's back in order
+    for i in range(0, len(bucket.events) - 1):
+        assert bucket.events[i].timestamp <= bucket.events[i + 1].timestamp
+
+
+def test_BucketFarm_add():
+    farm = BucketFarm()
+    for name, ix, should_add in (
+        ("2018-07", 0, True),
+        ("2018-06", 0, True),
+        ("2017-12", 0, True),
+        ("2018-07", 2, False),
+        ("2020-11", 3, True),
+    ):
+        res = farm._add_bucket(name)
+        assert res[0].bucket_name == name
+        assert res[1] == ix
+        assert res[2] == should_add
 
 
 if __name__ == "__main__":
+    test_BucketFarm_add()
+    test_load_bucket()
+    test_fake_90days()
     test_load_raw_log()
