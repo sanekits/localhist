81c4ea57f0f37ed40029cc538e22be33e1ac2b1f
Les Matheson <lmatheson4@bloomberg.net>
2022-12-05 18:10:23 -0500

    Bump for new shellkit

--- bin/localhist-version.sh
+++ bin/localhist-version.sh
@@ -2,7 +2,7 @@
 
 # Running localhist-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=2.5.7
+KitVersion=2.5.8
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
